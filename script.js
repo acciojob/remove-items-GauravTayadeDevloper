3a8da818529e2fedc132103101a56c9c9e111b00
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-26 11:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  let select = document.getElementById("colorSelect");
+  let button = document.querySelector("input[type='button']");
+
+  button.addEventListener("click", () => {
+    // Remove the currently selected option
+    if (select.selectedIndex !== -1) {
+      select.remove(select.selectedIndex);
+    } else {
+      alert("No more options to remove!");
+    }
+  });
+});
